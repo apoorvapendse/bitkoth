7bc4465d5727aa37170fb6b4b2ab361985f6b4d2
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-12-13 20:45:32 +0530

    CORS fix

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { router } from './router/router.js';
 import { connectDB } from './database/db.js';
+
+
+
+const app = express();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-
-
-
-const app = express();
 
 app.use(express.json());
 
