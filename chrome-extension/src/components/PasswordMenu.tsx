c4d82cbd216e829bef3bbc7ae9d6a165c7ccfd0f
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-12-14 18:45:31 +0530

    password generator begin

--- chrome-extension/src/components/PasswordMenu.tsx
+++ chrome-extension/src/components/PasswordMenu.tsx
@@ -1,12 +1,40 @@
+import { useState } from 'react'
+import '../menu.css'
+
 interface PropType{
     passwordName:string,
     passwordValue:string,
     mail:string
 }
+
+
 const PasswordMenu = ({}:PropType) => {
+
+    const[passwordLength,setPasswordLength] = useState(8);
+
   return (
-    <div>
-        PasswordMenu
+    <div className="container">
+        <form>
+    
+    <div className="group">      
+      <input type="text" required/>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Service Name/URL</label>
+    </div>
+      
+    <div className="group">      
+      <input type="text" required/>
+      <span className="highlight"></span>
+      <span className="bar"></span>
+      <label>Password</label>
+    </div>
+
+    <input type="range" min={8} max={32} onChange={(e)=>setPasswordLength(Number(e.target.value))}/>
+
+    <button>Save</button>
+    
+  </form>
     </div>
   )
 }
