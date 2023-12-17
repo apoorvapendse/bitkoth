c9cb74d4312712da624e95b96b1520fd3875cb4a
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-12-17 18:24:24 +0530

    encrypt decrypt passwords

--- backend/controllers/getPasswords.js
+++ backend/controllers/getPasswords.js
@@ -1,33 +1,49 @@
 import { Users } from "../model/User.js";
+import crypto from 'crypto'
 import bcrypt from 'bcrypt'
-export async function getPasswords(req,res){
-    const{email,masterPassword} = req.body;
+
+async function decryptPass(encryptedPass) {
+    var decipher = crypto.createDecipheriv(process.env.ALGO, process.env.SECRET_KEY, process.env.IV);
+    var decrypted = decipher.update(encryptedPass, 'hex', 'utf8') + decipher.final('utf8');
+    return decrypted;
+}
+
+export async function getPasswords(req, res) {
+    const { email, masterPassword } = req.body;
     console.log(req.body);
-    const currUser = await Users.findOne({email:email});
+    const currUser = await Users.findOne({ email: email });
 
-    console.log(currUser);
-    if(currUser){
+    if (currUser) {
         try {
-            console.log("bcrypt compare result:",await bcrypt.compare(masterPassword,currUser.masterPassword)); 
-            if(await bcrypt.compare(masterPassword,currUser.masterPassword)){
+            console.log("bcrypt compare result:", await bcrypt.compare(masterPassword, currUser.masterPassword));
+            if (await bcrypt.compare(masterPassword, currUser.masterPassword)) {
+                // Use map and Promise.all to handle asynchronous operations
+                const decryptedPasswords = await Promise.all(currUser.savedPasswords.map(async (item,index) => {
+                    console.log(item);
+                    const dpass = await decryptPass(item.password);
+                    console.log(dpass);
+                    currUser.savedPasswords[index].password = dpass; 
+                    return dpass;
+                }));
 
-                console.log({ passwords: currUser.savedPasswords });
-                res.status(200).json({passwords: currUser.savedPasswords});
-            }else{
+                console.log(decryptedPasswords);
+
+                
+                res.status(200).json({ passwords: currUser.savedPasswords });
+            } else {
                 res.status(403).json({
-                    message:"invalid password"
-                })
+                    message: "Invalid password"
+                });
             }
 
         } catch (error) {
             res.status(500).json({
-                error:error
-            })
+                error: error
+            });
         }
-    }
-    else{
+    } else {
         res.status(404).json({
-            message:"User does not exist"
-        })
+            message: "User does not exist"
+        });
     }
 }
